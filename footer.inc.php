e562ef420b305a8b518835cde13a4c6c74251f37
lglfrey <isip_o.d.petrov@mpt.ru>
2023-05-25 11:19:48 +0300

    php

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <h2>Подвал...</h2>
+</div>
---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#"> О нас </a></li>
+        <li><a href="#"> Портфолио </a></li>
+        <li><a href="#"> Заметки </a></li>
+        <li><a href="#"> Ссылки </a></li>
+        <li><a href="#"> Контакты </a></li>
+    </ul>
+</nav>
